99d9b425aab7c71fa0ef46a8f76ed54f80987c6b
m-inic <minic1300@gmail.com>
2025-04-29 02:28:24 +0200

    GOTOVO

--- guessing-game-timer/app.js
+++ guessing-game-timer/app.js
@@ -11,6 +11,11 @@ let clickedImg = '';
 let matchingImgs = false;
 let counterInterval;
 let matchCount = 0;
+const picPathName = [
+    "crunchyCat.jpg", "cryingCat.jpg", "grinchMatcha.jpg", "heavyCat.jpg",
+    "icedMatcha.jpg", "lavandMatcha.jpg", "mangoMatcha.jpg", "matchaChia.jpg",
+    "sleeperCat.jpg", "sorryCat.jpg", "strawberryMatcha.jpg", "tough-life-of-a-street-cat-v0-32m4rxvp44sa1.jpg"
+]
 
 function getFirstImg() {
     firstImg = clickedImg;
@@ -37,27 +42,51 @@ function startTimer() {
     if (timerStarted) return;
     timerStarted = true;
 
-    if (+timeNumber.innerHTML === 30) {
+    if (+timeNumber.innerHTML === 45) {
         counterInterval = setInterval(() => {
-            timeNumber.innerHTML = +timeNumber.innerHTML - 1
+            timeNumber.innerHTML = +timeNumber.innerHTML - 45;
             if (+timeNumber.innerHTML <= 0) {
                 clearInterval(counterInterval);
-                timeNumber.innerHTML = "Your time is up! Try again!"
-                secs.textContent = secs.textContent.slice(0, -1);
+                timeNumber.innerHTML = "Your time is up! Try again!";
                 listOfImgs.style.pointerEvents = "none";
+                timerStarted = false;
             }
         }, 1000)
+    }
+    timeNumber.innerHTML = 45;
+}
+
+// TREBA DA NAPRAVIM PREV I CUR I DA UPOREDJUJEM, I TREBA KAD ISTEKNE VREME DA NE MOZE DA SE KLIKCE
+
+function randomizePlacement() {
+    let imgPool = []
+    picPathName.forEach(name => {
+        imgPool.push(name);
+        imgPool.push(name);
+    });
+
+    imgPool.sort(() => Math.random() - 0.5);
+
+    for (let i = 0; i < 24; i++) {
+        gamePics[i].src = `pics/game/${imgPool[i]}`
+        gamePics[i].style.opacity = 0;
+        gamePics[i].style.pointerEvents = "auto";
+        console.log(gamePics[i]);
     }
 }
 
 function matchImgs(event) {
+    if (timeNumber.innerHTML === "Your time is up! Try again!") {
+        return
+    };
+    
     startTimer();
     if (event.target.nodeName === "IMG") {
         clickedImg = event.target;
     } else if (clickedImg.nodeName !== "IMG" || clickedImg === firstImg || clickedImg.style.opacity == 1) {
         return;
     }
-    
+
     if (!firstImg) {
         getFirstImg();
         firstImg.style.opacity = 1;
@@ -80,8 +109,8 @@ function matchImgs(event) {
             matchCount += 2;
             console.log("its a match");
         }
-        
-        if(matchCount === gamePics.length) {
+
+        if (matchCount === gamePics.length) {
             clearInterval(counterInterval)
             timeNumber.innerHTML = "Congratulations!!! You won! Try again maybe?";
             listOfImgs.style.pointerEvents = "none";
@@ -98,9 +127,12 @@ function restartGame(event) {
         gamePics[i].style.pointerEvents = "auto";
         gamePics[i].style.opacity = 0;
     }
+
+    listOfImgs.style.pointerEvents = "auto";
     resetSelection();
-    timeNumber.innerHTML = 30;
+    timeNumber.innerHTML = 45;
     timerStarted = false;
+    randomizePlacement();
 }
 
 resetBtn.addEventListener("click", restartGame);
