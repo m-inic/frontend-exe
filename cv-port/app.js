ebeb60ec75a9b717aaf3e295fa336f815293d387
m-inic <minic1300@gmail.com>
2025-06-15 23:52:40 +0200

    hide/show not done

--- cv-port/app.js
+++ cv-port/app.js
@@ -7,19 +7,25 @@ const getToContact = document.getElementById("nav-menu-link-3");
 const hiEmoji = document.getElementById("hi-emoji");
 const toTopBtn = document.getElementById("to-top-btn");
 const blackText = document.querySelectorAll("#assume span");
+const circleMenuBtn = document.getElementById("circle-menu");
+const linksList = document.querySelector("#link-frames ul");
+const listLink1 = linksList.children[0].children[0]
+const listLink2 = linksList.children[1].children[0]
+const listLink3 = linksList.children[2].children[0]
+const overlay = document.getElementById("overlay");
 
 function nextImage() {
-    if(window.innerWidth < 1280) {
+    if (window.innerWidth < 1280) {
         scrollContainer.scrollLeft += 610;
-    } else if(window.innerWidth >= 1280) {
+    } else if (window.innerWidth >= 1280) {
         scrollContainer.scrollLeft += 786;
     }
 }
 
 function prevImage() {
-    if(window.innerWidth < 1280) {
+    if (window.innerWidth < 1280) {
         scrollContainer.scrollLeft -= 610;
-    } else if(window.innerWidth >= 1280) {
+    } else if (window.innerWidth >= 1280) {
         scrollContainer.scrollLeft -= 786;
     }
 }
@@ -57,10 +63,10 @@ function shakeHand() {
 
 hiEmoji.addEventListener("click", shakeHand);
 
-window.onscroll = function() {showToTopBtn()};
+window.onscroll = function () { showToTopBtn() };
 
 function showToTopBtn() {
-    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         toTopBtn.style.display = "flex";
         toTopBtn.style.opacity = 1;
     } else {
@@ -75,10 +81,31 @@ toTopBtn.addEventListener("click", () => {
     document.documentElement.scrollTop = 0;
 })
 
-if(window.innerWidth >= 1280) {
+if (window.innerWidth >= 1280) {
     blackText[0].style.color = "white";
     blackText[1].style.color = "white";
-} else if(window.innerWidth < 1280) {
+} else if (window.innerWidth < 1280) {
     blackText[0].style.color = "black";
     blackText[1].style.color = "black";
 }
+
+function showMenu() {
+    linksList.style.display = "flex";
+    listLink1.classList.add("animate__fadeInTopRight");
+    listLink2.classList.add("animate__fadeInDown");
+    listLink3.classList.add("animate__fadeInTopLeft");
+    overlay.style.display = "block";
+}
+
+function hideMenu() {
+    linksList.style.display = "none";
+    listLink1.classList.replace("animate__fadeInTopRight", "animate__fadeOutTopRight");
+    listLink2.classList.replace("animate__fadeInDown", "animate__fadeOutUp");
+    listLink3.classList.replace("animate__fadeInTopLeft", "animate__fadeOutTopLeft");
+    overlay.style.display = "none";
+}
+
+// TREBA DA DORADIM ANIMACIJU
+
+circleMenuBtn.addEventListener("click", showMenu);
+overlay.addEventListener("click", hideMenu)
